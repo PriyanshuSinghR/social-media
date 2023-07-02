3e8643538a5b1c1da51bc184dc93db4754476696
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2023-07-02 11:45:46 +0530

    final deploy

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -8,7 +8,8 @@ import { formatDate } from '../utils/authUtils';
 export const posts = [
   {
     _id: 'cgcgcgcgcggc',
-    content: 'At vero eos et accusamus et iusto odio dignissimos .',
+    content:
+      'Quote of the Day </br> ANIME MAKES ME HAPPY WHEN NO ONE ELSE DOES',
     mediaURL: '',
     likes: {
       likeCount: 0,
@@ -17,40 +18,90 @@ export const posts = [
     },
     comments: [],
     username: 'adarshbalika',
-    createdAt: formatDate(),
+    createdAt: '2023-01-13T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'ytytyttyty',
-    content: 'At vero eos et accusamus et iusto odio dignissimos .',
+    content: 'Itachi is a masterpiece',
     mediaURL: 'https://motionbgs.com/media/945/itachi-uchiha-in-autumn.jpg',
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    comments: [],
+    comments: [
+      {
+        _id: 'gygyygyggyyg',
+        comment: 'It is more than master piece',
+        firstName: 'Priyanshu',
+        lastName: 'Singh',
+        username: 'priyanshu_singh',
+        image:
+          'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        createdAt: formatDate(),
+        updatedAt: formatDate(),
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
     username: 'shubhamsoni',
-    createdAt: formatDate(),
+    createdAt: '2022-01-28T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'oopoppopoo',
-    content: 'At vero eos et accusamus et iusto odio dignissimos .',
+    content:
+      'Suggest manga/anime for me </br>I LOVED one piece, bleach, black clover, dragon ball (db,dbz,dbs), yuyu hakusho, one punch man, and vinland saga. want some more suggestions. im reading/watching attack on titan and hated hxh so dont suggest.',
     mediaURL: '',
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    comments: [],
+    comments: [
+      {
+        _id: 'dededed',
+        comment: 'Bro no doubt, go for one piece',
+        firstName: 'Priyanshu',
+        lastName: 'Singh',
+        username: 'priyanshu_singh',
+        image:
+          'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+        createdAt: formatDate(),
+        updatedAt: formatDate(),
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+      {
+        _id: 'sasasakkllk',
+        comment:
+          'Black Clover is also awsome bro , you can take that on list also',
+        firstName: 'Sam',
+        lastName: 'Song',
+        username: 'sam_song',
+        image:
+          'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        createdAt: formatDate(),
+        updatedAt: formatDate(),
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+      },
+    ],
     username: 'overlord_wrath',
-    createdAt: formatDate(),
+    createdAt: '2021-07-05T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'rtrtrtrtrt',
-    content: 'At vero eos et accusamus et iusto odio dignissimos .',
+    content:
+      'Madara vs Sakura </br>I searched it on Google 🤣🤣 Google says Sakura the trash wins😂😂🤣',
     mediaURL: '',
     likes: {
       likeCount: 0,
@@ -59,12 +110,13 @@ export const posts = [
     },
     comments: [],
     username: 'overlord_wrath',
-    createdAt: formatDate(),
+    createdAt: '2023-06-28T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'xzxzxzxz',
-    content: 'At vero eos et accusamus et iusto odio dignissimos .',
+    content:
+      'Who played Pokemon fire red and leaf green and other Pokemon games? Those were awesome 😍',
     mediaURL: '',
     likes: {
       likeCount: 0,
@@ -73,14 +125,14 @@ export const posts = [
     },
     comments: [],
     username: 'sam_song',
-    createdAt: formatDate(),
+    createdAt: '2023-03-18T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'asasasasa',
-    content:
-      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias .',
-    mediaURL: '',
+    content: '😂😂 funny',
+    mediaURL:
+      'https://images.animefansbase.com/timeline/md/2023/07/60954-64a0cadd38927.jpeg',
     likes: {
       likeCount: 0,
       likedBy: [],
@@ -88,13 +140,12 @@ export const posts = [
     },
     comments: [],
     username: 'priyanshu_singh',
-    createdAt: formatDate(),
+    createdAt: '2022-08-17T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
     _id: 'wqwqwqwq',
-    content:
-      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis. ',
+    content: `It's okay to not have everything figured out </br>Just take things one step at a time. `,
     mediaURL: '',
     likes: {
       likeCount: 0,
@@ -103,7 +154,7 @@ export const posts = [
     },
     comments: [],
     username: 'jatin_smart',
-    createdAt: formatDate(),
+    createdAt: '2023-02-21T10:55:06+05:30',
     updatedAt: formatDate(),
   },
   {
@@ -154,7 +205,7 @@ export const posts = [
   },
   {
     _id: 'siFFxfYI1s',
-    content: 'Started my Baking journey! Look what I made.',
+    content: 'Time is everything',
     mediaURL:
       'https://w0.peakpx.com/wallpaper/146/580/HD-wallpaper-one-piece-shanks-one-piece-thumbnail.jpg',
     likes: {
@@ -164,6 +215,21 @@ export const posts = [
     },
     comments: [],
     username: 'adarshbalika',
+    createdAt: '2022-05-21T10:55:06+05:30',
+    updatedAt: formatDate(),
+  },
+  {
+    _id: 'cygchgcchgghcghc',
+    content: '😂😂',
+    mediaURL:
+      'https://images.animefansbase.com/timeline/md/2023/07/29202-64a016f15d53e.jpeg',
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comments: [],
+    username: 'jatin_smart',
     createdAt: '2022-05-21T10:55:06+05:30',
     updatedAt: formatDate(),
   },
